ee99a6a5827b8813b55e1fd8322bbf03a0875fb5
DESKTOP-I35MX45\Parsa <rahmaty.parsa@gmail.com>
2021-12-03 21:55:50 +0330

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Saving the last entered name in this variable
 var lastEnteredName;
 
 // Restricts input for the given textbox to the given inputFilter.
@@ -18,12 +19,14 @@ function setInputFilter(textbox, inputFilter) {
     });
 }
 
+// Checks if the name textbox only contains latin characters and whitespaces
 function checkInputBox() {
     setInputFilter(document.getElementById("name"), function (value) {
         return /^[a-zA-Z\s]*$/i.test(value);
     });
 }
 
+// Called when the submit button is pressed. Sends the name via the API and prints the results on page.
 function submit() {
     var name = document.getElementById('name').value;
     lastEnteredName = name;
@@ -44,6 +47,7 @@ function submit() {
     }
 }
 
+// Called when the clear button is pressed. Clears the saved gender in the localStorage.
 function clearButton() {
     if (getSavedResult(lastEnteredName)) {
         localStorage.removeItem(lastEnteredName)
@@ -51,10 +55,12 @@ function clearButton() {
     document.getElementById("savedGender").innerHTML = "";
 }
 
+// Fetch result from API for the given name
 function fetchAPI(name) {
     return fetch("https://api.genderize.io/?name=" + name)
 }
 
+// Saves a gender for a name on localStorage
 function saveGender() {
     var name = document.getElementById('name').value;
     if (!name) {
@@ -72,6 +78,7 @@ function saveGender() {
     localStorage.setItem(name, gender);
 }
 
+// Searches the localStorage and returns the gender saved for the given name
 function getSavedResult(name) {
     for (var i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) !== name) {
